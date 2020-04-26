aa2f49f9f883d2168b02d460ba573c5ee7fc3b2c
MiruE <honoka@live.it>
2020-04-26 19:12:47 +0200

    Commit from test branch

--- app/src/main/java/com/mirue/test/MainActivity.java
+++ app/src/main/java/com/mirue/test/MainActivity.java
@@ -13,7 +13,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+// A comment from test brench.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
